bf5ec2f971d079d4ae5e7ca3f65dade91eed358b
Kacper Harabasz <kharabasz@chathamfinancial.eu>
2015-09-25 07:02:27 -0400

    initial commit

---
+++ SelfHost1/SelfHost1/AppHost.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Funq;
+using ServiceStack;
+
+namespace SelfHost1
+{
+    [Route("/request")]
+    public class Request : IReturn<Response>{}
+    public class Response{}
+
+    public class MyServices : Service
+    {
+        public object Any(Request request)
+        {
+            return new Response();
+        }
+    }
+
+    public interface IDependency
+    {
+        Guid GetInstanceId();
+    }
+
+    public class SomeDependency : IDependency//, IDisposable
+    {
+        private readonly Guid _id = Guid.NewGuid();
+
+        public Guid GetInstanceId()
+        {
+            return _id;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing: " + _id);
+        }
+    }
+
+    public class AppHost : AppHostHttpListenerPoolBase
+    {
+        /// <summary>
+        /// Default constructor.
+        /// Base constructor requires a name and assembly to locate web service classes. 
+        /// </summary>
+        public AppHost()
+            : base("SelfHost1", typeof(MyServices).Assembly)
+        {
+        }
+
+        /// <summary>
+        /// Application specific configuration
+        /// This method should initialize any IoC resources utilized by your web service classes.
+        /// </summary>
+        /// <param name="container"></param>
+        public override void Configure(Container container)
+        {
+            container.RegisterAs<SomeDependency, IDependency>().ReusedWithin(ReuseScope.Request);
+
+            //if (container.TryResolve<IDependency>() != default(IDependency))
+            //{
+            //}
+
+            GlobalRequestFilters.Add((req, res, requestDto) =>
+            {
+                var thatDependency = container.Resolve<IDependency>();
+                Console.WriteLine("Filter received: " + thatDependency.GetInstanceId());
+            });
+        }
+    }
+}
---
+++ SelfHost1/SelfHost1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using ServiceStack.Text;
+
+namespace SelfHost1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AppHost().Init().Start("http://*:8088/");
+            "ServiceStack SelfHost listening at http://localhost:8088 ".Print();
+            Process.Start("http://localhost:8088/");
+
+            Console.ReadLine();
+        }
+    }
+}
